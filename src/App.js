b8e992b6040c99335b603f4bec0d1c84dbe9fc01
konok101 <rockykonok@gmail.com>
2022-08-30 20:40:14 +0600

    navbar design part done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Navbar from './Pages/Shared/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div>
 
-      <button class="btn btn-primary">Button</button>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='/login' element={<Login/>}/>
+
+      </Routes>
     </div>
   );
 }
