c11d024b46439e83cc2fb7b006effbbba12c64f9
konok101 <rockykonok@gmail.com>
2022-09-08 10:53:39 +0600

    email and password validation form done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
 import auth from '../../firebase.init';
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
 
     if (user) {
         console.log(user);
     }
-    else{
-        console.log('as');
+    
+    const onSubmit = (data) => {
+        console.log('assa',data);
+
     }
 
 
@@ -18,6 +23,76 @@ const Login = () => {
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className=" text-center text-2xl font-bold">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div class="form-control w-full max-w-xs">
+
+                            <label class="label">
+                                <span class="label-text" >Email</span>
+
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="email here"
+                                class="input input-bordered w-full max-w-xs"
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: 'email is required'
+
+                                    },
+                                    pattern: {
+                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                        message: 'Provide a valid email'
+                                    }
+                                })}
+
+                            />
+
+                            <label class="label">
+                                {errors.email?.type === 'required' && <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+
+
+                            </label>
+                        </div>
+                        <div class="form-control w-full max-w-xs">
+
+                            <label class="label">
+                                <span class="label-text" >Password</span>
+
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="password here"
+                                class="input input-bordered w-full max-w-xs"
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: 'password is required'
+
+                                    },
+                                    minLength: {
+                                        value: 6,
+                                        message: ' Must be 6 characters or longer a '
+                                    }
+                                })}
+
+                            />
+
+                            <label class="label">
+                                {errors.password?.type === 'required' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+                                {errors.password?.type === 'pattern' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+
+
+                            </label>
+                        </div>
+
+
+                        <input className='btn w-full max-w-xs ' type="submit" />
+                    </form>
+
+
+                    <div class="divider">OR</div>
                     <button
                         onClick={() => signInWithGoogle()}
                         className="btn btn-outline uppercase"> Continue with google</button>
